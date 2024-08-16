10230cd7486644780248965867bca603bc158fa5
bamos guinley <guinleyb@gmail.com>
2024-08-16 13:43:41 +0200

    add base operations

---
+++ src/app/operation/operation.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operation',
+  templateUrl: './operation.component.html',
+  styleUrl: './operation.component.css'
+})
+export class OperationComponent {
+ operator:string[]=["+","-","*","/"];
+ firstMember:number=0;
+ secondMember:number=0;
+ currentOperator:string='';
+ currentResult:number=0;
+ userInput!:string;
+ countFind:number=0;
+ opertionInterval:any;
+time:number=10;
+oparationNumber:number=10;
+ngOnInit(){
+  this.currentOperator=this.operator[Math.floor(Math.random()*this.operator.length)];
+  this.firstMember= Math.floor(Math.random()*9)
+  this.secondMember= Math.floor(Math.random()*9)
+  switch(this.currentOperator){
+    case '-':
+    if (this.firstMember<this.secondMember) {
+      this.createOperation();
+    }
+    break;
+   case '*':
+    if (this.firstMember===0 && this.secondMember===0) {
+      this.createOperation();
+    }
+    break;
+    case'/':
+    if (this.secondMember===0 || this.firstMember===0 || (this.firstMember/this.secondMember)%2!==0) {
+      this.createOperation();
+    }
+ }
+
+}
+ createOperation(){
+  this.currentOperator=this.operator[Math.floor(Math.random()*this.operator.length)];
+  this.firstMember= Math.floor(Math.random()*9)
+  this.secondMember= Math.floor(Math.random()*9)
+
+  console.log(this.firstMember+this.currentOperator+ this.secondMember);
+  
+ }
+ makeOperation(){
+  switch(this.currentOperator){
+    case '+':
+      this.currentResult=this.firstMember+this.secondMember; 
+      break;
+    case '-':
+    this.currentResult=this.firstMember-this.secondMember ;
+    break;
+    case '*': 
+    this.currentResult=this.firstMember*this.secondMember ;
+    break;
+    case '/':
+      this.currentResult=this.firstMember/this.secondMember ;
+      break;
+      default: 
+      console.log("error");
+ }
+}
+showResult(){
+  this.makeOperation();
+if (this.oparationNumber>0) {
+  if (this.currentResult==parseInt(this.userInput)) {
+    this.countFind++;
+    this.userInput='';
+    this.createOperation();
+  } else{
+    this.countFind=this.countFind
+    this.userInput='';
+    this.createOperation();
+  }
+  this.oparationNumber--;
+}
+}}
